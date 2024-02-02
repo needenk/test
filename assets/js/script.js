a74c812a4948af97974e61f5380d9c7e50bc967f
needenk <needenk@gmail.com>
2024-02-02 19:16:45 -0300

    xsd and some fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-var xhttp = new XMLHttpRequest();
+function callxhtpp(file, isJSON, callback){
+	var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		testFunction(this);
+	if(isJSON){
+		xhttp.overrideMimeType("application/json");
 	}
-};
+	
+	xhttp.open("GET", file, true);
 
-xhttp.open("GET", "test.xml", true);
-xhttp.send();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			callback(xhttp);
+		}
+	}
+	xhttp.send();
+}
 
-function testFunction(xml) {
+callxhtpp("test.xml", false, function(xml){
 	var xmlDoc = xml.responseXML;
 	document.getElementById("testParagraph").innerHTML =
 		xmlDoc.getElementsByTagName("testing")[0].childNodes[0].nodeValue;
-}
+});
+
+callxhtpp("testJSON.json", true, function(testJSON){
+	let obj = JSON.parse(testJSON.responseText);
+	document.getElementById("testParagraphJSON").innerHTML = obj.testing;
+});
